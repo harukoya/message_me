32eaba7338e816eab1cfcf5f8f148634b24824b3
harukoya <ka.uni.stu@gmail.com>
2021-08-11 18:52:46 +0900

    Add production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,6 +46,9 @@ Rails.application.configure do
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
+  config.action_cable.url = 'wss://message-app-haru.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = ['https://message-app-haru.herokuapp.com', 'http://message-app-haru.herokuapp.com']
+
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
